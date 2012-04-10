e3a3d740468badd93ae77dbad58e6578b7ba0a9a
Andrew Haigh <nelfin@gmail.com>
2012-04-10 18:28:58 +1000

    Completed connection code and data marshalling

--- org.nelfin.othello/src/iago/Client.java
+++ org.nelfin.othello/src/iago/Client.java
@@ -1,14 +1,26 @@
 package iago;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 
 public class Client {
     
     private static final String DEFAULT_HOST = "localhost";
     private static final int DEFAULT_PORT = 3130;
+    private static final String NAME = "Iago";
     
     private String host;
     private int port;
     private Board.Player player;
+    private Socket socket;
+    private DataOutputStream out;
+    private BufferedReader in;
     
     /**
      * @param args
@@ -45,8 +57,51 @@ public class Client {
         }
         
         // And, we're off!
-        @SuppressWarnings("unused")
         Client client = new Client(player, host, port);
+        if (!client.connect()) {
+            System.exit(1);
+        }
+        client.runForever();
+    }
+
+    private boolean connect() {
+        try {
+            this.socket = new Socket(this.host, this.port);
+            this.out = new DataOutputStream(this.socket.getOutputStream());
+            this.in = new BufferedReader(new InputStreamReader(
+                    this.socket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("[client] unknown host: " + this.host);
+            return false;
+        } catch (IOException e) {
+            System.err.println("[client] error creating socket");
+            return false;
+        }
+        try {
+            this.out.write(connectMessage());
+            this.out.flush();
+        } catch (IOException e) {
+            System.err.println("[client] could not connect to server");
+            return false;
+        }
+        return true;
+    }
+
+    private byte[] connectMessage() throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        DataOutputStream w = new DataOutputStream(buffer);
+        
+        w.writeInt(this.player.toInteger());
+        w.writeInt(NAME.length());
+        w.write(NAME.getBytes());
+        w.flush();
+        
+        return buffer.toByteArray();
+    }
+
+    private void runForever() {
+        // TODO Auto-generated method stub
+        
     }
 
     public Client(Board.Player player, String host, int port) {
