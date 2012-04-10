411724492706c3979abc3e909e2407081f0081b7
Andrew Haigh <nelfin@gmail.com>
2012-04-10 18:28:19 +1000

    Added conversion methods to enum Player

--- org.nelfin.othello/src/iago/Board.java
+++ org.nelfin.othello/src/iago/Board.java
@@ -1,6 +1,28 @@
 package iago;
 
 public class Board {
-    public static enum Player { NONE, WHITE, BLACK };
+    public static enum Player {
+        WHITE, BLACK, NONE;
+        
+        public int toInteger() {
+            switch (this) {
+            case WHITE:
+                return 0;
+            case BLACK:
+                return 1;
+            }
+            return 2;
+        }
+
+        public static long toInteger(Player p) {
+            switch (p) {
+            case WHITE:
+                return 0;
+            case BLACK:
+                return 1;
+            }
+            return 2;
+        }
+    }
     
 }
