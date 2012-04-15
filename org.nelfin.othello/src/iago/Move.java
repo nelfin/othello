c771695289b2b2daca0d16f3b73049426f2f65bd
Andrew Haigh <nelfin@gmail.com>
2012-04-15 15:12:41 +1000

    Added default "no" move

--- org.nelfin.othello/src/iago/Move.java
+++ org.nelfin.othello/src/iago/Move.java
@@ -7,7 +7,9 @@ public class Move extends Point {
      * 
      */
     private static final long serialVersionUID = 2066875822332872611L;
-
+    
+    public static final Move NO_MOVE = new Move(-1, -1);
+    
     public Move(int x, int y) {
         super(x, y);
     }
