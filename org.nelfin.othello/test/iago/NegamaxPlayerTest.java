fa8c02ea4772c1dfdcfbeefe9674286473628f71
Andrew Haigh <nelfin@gmail.com>
2012-04-21 20:27:50 +1000

    Avoid using move ordering heuristics

--- org.nelfin.othello/test/iago/NegamaxPlayerTest.java
+++ org.nelfin.othello/test/iago/NegamaxPlayerTest.java
@@ -2,8 +2,8 @@ package iago;
 
 public class NegamaxPlayerTest extends SmartPlayerTest {
 	protected void setUp() {
-		smartWhitePlayer = new NegamaxPlayer(Player.PlayerType.WHITE, 8);
-		smartBlackPlayer = new NegamaxPlayer(Player.PlayerType.BLACK, 8);
+		smartWhitePlayer = new NegamaxPlayer(Player.PlayerType.WHITE, 8, 0);
+		smartBlackPlayer = new NegamaxPlayer(Player.PlayerType.BLACK, 8, 0);
 		greedyPlayer = new NegamaxPlayer(Player.PlayerType.WHITE, 1);
     }
 	
