15d70f1d4d404890f002507fde118a191126fc1b
Tim Cosgrove <tim.rccosgrove@gmail.com>
2012-05-06 13:54:53 +1000

    Testing out how to load weights and features from a file.
    Uses a heckload of unsafe operations; resulting in the most
    incredible try-catch block ever.

--- org.nelfin.othello/src/iago/Feature.java
+++ org.nelfin.othello/src/iago/Feature.java
@@ -2,22 +2,33 @@ package iago;
 
 public abstract class Feature {
 
-	public String name;
-	
-	public String description;
-	
 	private double weight;
 	
-	public Feature (String name, String description, double weight) {
+	public final double defaultWeight;
+	public final String description;
+	public final String name;
+	
+	//Feature classes should pass static hard-coded constants for first 3.
+	public Feature (String name, String description, double defaultWeight, double weight) {
 		this.name        = name;
 		this.description = description;
-		this.weight      = weight;
+		this.weight = weight;
+		this.defaultWeight = defaultWeight;
 	}
 	
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
 	//Many features may wish to override this and scale their weights rather than
 	//direct over-write
+	//Maybe should be aware of win/loss state...
 	public void updateWeight(double weight) {
-		this.weight = weight;
+		this.setWeight(weight);
 	}
 	
 	public abstract Integer evaluate(Board state);
--- org.nelfin.othello/src/iago/FeatureSet.java
+++ org.nelfin.othello/src/iago/FeatureSet.java
@@ -1,13 +1,21 @@
 package iago;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
 public class FeatureSet {
 
 	private ArrayList<Feature> Features;
+	private String playerID;
 	
-	public FeatureSet (ArrayList<Feature> Features) {
+	public FeatureSet (ArrayList<Feature> Features, String playerID) {
 		this.Features = Features;
+		this.playerID = playerID;
 	}
 	
 	public void add (Feature f) {
@@ -28,5 +36,48 @@ public class FeatureSet {
 	//A game has just ended, evaluate and adjust features based on Win/Loss
 	public void update (FeatureSet opponent, Board finalstate) {
 		//Learn how to learn
+	}
+
+	public void load () {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(playerID + ".pl"));
+			String featureLn;
+			String fname;
+			double fweight;
+			while ((featureLn = br.readLine()) != null)   {
+				fname   = featureLn.split(":")[0];
+				fweight = Double.parseDouble(featureLn.split(":")[1]);
+				Constructor<?> fcons = Class.forName(fname).getConstructor(new Class[]{Double.TYPE});
+				Feature f = (Feature) fcons.newInstance(new Object[]{fweight});
+				Features.add(f);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
 }
