d622a6be34da5089ab3ecdfd7b0207f9853d4175
Joshua Nelson <cyberdash@gmail.com>
2012-05-09 22:45:44 +1000

    Quickly wrote a legal moves feature

---
+++ org.nelfin.othello/src/iago/features/LegalMoves.java
@@ -0,0 +1,28 @@
+package iago.features;
+
+import java.util.HashSet;
+
+import iago.Board;
+import iago.players.*;
+
+public class LegalMoves extends Feature{
+	
+	private HashSet<Integer[]> blockedPoints = new HashSet<Integer[]>();
+	private Integer[][] visibilityMap = new Integer[Board.BOARD_SIZE][Board.BOARD_SIZE];
+	private boolean visibilityMapInitialised = false;
+	public LegalMoves(double weight)
+	{
+		super("LegalMoves", "The number of legal moves for white", weight);
+	}
+	
+	
+	
+	/**
+	 * The number of legal moves for the next player
+	 * @return Legal moves white
+	 */
+	public Integer evaluate(Board state)
+	{
+		return state.validMoves(Player.PlayerType.WHITE).size();
+	}
+}
