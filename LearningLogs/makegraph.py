2e41924a7f8590d1c82998e9edca347b887ae531
Tim Cosgrove <tim.rccosgrove@gmail.com>
2012-05-13 15:50:22 +1000

    Buggyfix
    Fixed a data ordering issue

--- LearningLogs/makegraph.py
+++ LearningLogs/makegraph.py
@@ -1,12 +1,9 @@
 #! /usr/bin/python
 import matplotlib.pyplot as plt
 import os
-
 os.system("cat LearningHistory_*.csv > combine.csv")
-x = []
-y = []
 with open("combine.csv", "r") as f:
-    [(x.append(i.split(",")[0]), y.append(i.split(",")[1])) for i in f]
-
-plt.plot(x,y)
+    data = [(int(i.split(",")[0]), float(i.split(",")[1])) for i in f]
+data.sort()
+plt.plot([i[0] for i in data],[i[1] for i in data])
 plt.show()
