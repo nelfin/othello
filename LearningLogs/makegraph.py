85fdafa32ebcfef2d35747946f2147eb83a5cfde
Andrew Haigh <nelfin@gmail.com>
2012-05-14 18:27:33 +1000

    Update makegraph.py
    Fix to take into account recent changes to PracticeArena log output
    files

--- LearningLogs/makegraph.py
+++ LearningLogs/makegraph.py
@@ -1,9 +1,16 @@
-#! /usr/bin/python
+#!/usr/bin/python
+
+import numpy as np
 import matplotlib.pyplot as plt
 import os
-os.system("cat LearningHistory_*.csv > combine.csv")
-with open("combine.csv", "r") as f:
-    data = [(int(i.split(",")[0]), float(i.split(",")[1])) for i in f]
-data.sort()
-plt.plot([i[0] for i in data],[i[1] for i in data])
+
+os.system("cat LearningHistory*.csv > combine.csv")
+
+with open('combine.csv') as f:
+    data = np.loadtxt(f, dtype={
+        'names': ('iteration', 'cumAvg', 'expMovAvg'),
+        'formats': ('i', 'd', 'd')}, delimiter=',', skiprows=1)
+
+plt.plot(data['iteration'], data['cumAvg'])
+plt.plot(data['iteration'], data['expMovAvg'])
 plt.show()
