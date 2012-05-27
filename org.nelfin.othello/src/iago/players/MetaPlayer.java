d54ab41f166bf09e73005873b459c53e8b56230f
Andrew Haigh <nelfin@gmail.com>
2012-05-28 08:28:53 +1000

    Add basic game stages to MetaPlayer

--- org.nelfin.othello/src/iago/players/MetaPlayer.java
+++ org.nelfin.othello/src/iago/players/MetaPlayer.java
@@ -1,43 +1,67 @@
 package iago.players;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Set;
-
 import iago.Board;
 import iago.Move;
-import iago.features.*;
-import iago.players.Player.PlayerType;
 
 public class MetaPlayer extends AbstractPlayer{
-	NegamaxPlayer negamaxPlayer;
+    public static final int DEFAULT_DEPTH = NegamaxPlayer.DEFAULT_DEPTH;
+    private enum Stage { EARLY, MID, LATE };
+    
+    Player earlyGamePlayer;
+    Player midGamePlayer;
+    Player lateGamePlayer;
 	OpeningBookPlayer openingBookPlayer;
+	
+	private Stage gameStage;
 	
 	private void instantiateOpeningBookPlayer(PlayerType colour){
 		openingBookPlayer = new OpeningBookPlayer(colour);
 	}
 	public MetaPlayer(PlayerType colour) {
-		super(colour);
-		negamaxPlayer = new NegamaxPlayer(colour);
-		instantiateOpeningBookPlayer(colour);
-		
+	    this(colour, DEFAULT_DEPTH);
     }
     
     public MetaPlayer(PlayerType colour, int depth) {
     	super(colour);
-		negamaxPlayer = new NegamaxPlayer(colour, depth);
+    	gameStage = Stage.EARLY;
+        earlyGamePlayer = new NegamaxPlayer(colour, depth);
+        midGamePlayer = new NegamaxPlayer(colour, depth);
+        lateGamePlayer = new NegamaxPlayer(colour, depth);
 		instantiateOpeningBookPlayer(colour);
     }
 
 	@Override
 	public Move chooseMove(Board board) {
+	    determineStage(board);
+	    System.out.println("MetaPlayer: game stage is " + gameStage);
 		Move openingMove = openingBookPlayer.chooseMove(board);
-		if(openingMove != Move.NO_MOVE){ //Also make sure that it's a legal move (blocked spaces)
+		if (openingMove != Move.NO_MOVE) { //Also make sure that it's a legal move (blocked spaces)
 			return openingMove;
-		}else{
-			return negamaxPlayer.chooseMove(board);
+		} else {
+		    switch (gameStage) {
+		    case EARLY:
+		        return earlyGamePlayer.chooseMove(board);
+		    case MID:
+		        return midGamePlayer.chooseMove(board);
+		    default:
+		        return lateGamePlayer.chooseMove(board);
+		    }
 		}
 	}
+	
+    private void determineStage(Board board) {
+        // TODO: make this less naive
+        int movesLeft = board.movesRemaining();
+        int maximumMoves = Board.BOARD_SIZE*Board.BOARD_SIZE - Board.BLOCKED_NUM - 4;
+        float fracRemaining = ((float) movesLeft) / ((float) maximumMoves);
+        if (fracRemaining > 0.667) {
+            gameStage = Stage.EARLY;
+        } else if (fracRemaining > 0.333) {
+            gameStage = Stage.MID;
+        } else {
+            gameStage = Stage.LATE;
+        }
+    }
 	
 	
 
