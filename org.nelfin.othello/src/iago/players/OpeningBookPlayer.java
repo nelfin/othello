6cff1eb8101de3e2e7e4e6660352f323c93c22f1
Joshua Nelson <cyberdash@gmail.com>
2012-05-27 15:48:21 +1000

    Made the opening book player

---
+++ org.nelfin.othello/src/iago/players/OpeningBookPlayer.java
@@ -0,0 +1,18 @@
+package iago.players;
+
+import iago.Board;
+import iago.Move;
+
+public class OpeningBookPlayer extends AbstractPlayer{
+
+	public OpeningBookPlayer(PlayerType colour) {
+		super(colour);
+	}
+
+	@Override
+	public Move chooseMove(Board board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
